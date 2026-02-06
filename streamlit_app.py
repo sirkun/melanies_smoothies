904e1821734c3283d14a7cb00124b68ba9343f20
sirkun <33220555+sirkun@users.noreply.github.com>
2026-02-06 13:40:16 +0700

    Remove API call and dataframe display for smoothies
    Removed the request to the Smoothiefroot API and the corresponding dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,8 @@ if options:
     ingredients_string = ''
     for fruit_chosen in options:
         ingredients_string += fruit_chosen + ' '
-        
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
     time_to_insert = st.button('Submit Order')
