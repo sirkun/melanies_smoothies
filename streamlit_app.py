d06278a8b8b879181696b99f64b0a26be5d2295d
sirkun <33220555+sirkun@users.noreply.github.com>
2026-02-06 14:07:40 +0700

    Comment out dataframe display and stop
    Comment out dataframe display and stop calls.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 options = st.multiselect(
     "Choose up to 5 ingredients",
     my_dataframe,
